464c587070aeabf1a0cf08da2097f498137e16b9
Patato <pathonscript@gmail.com>
2024-06-18 20:49:01 +0700

    added google fonts

---
+++ src/lib/colours.ts
@@ -0,0 +1,8 @@
+export default {
+	primary: '3362A8',
+	secondary: 'FF8C22',
+	accent: '71AAFF',
+	softBlack: '212121',
+	softWhite: 'FEFEFE',
+	softGrey: 'B4B4B4'
+};
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '../styles/globals.scss';
+</script>
+
+<slot />
