16eaec3426c0e3b866d67bd7df4cad97f62770aa
Patato <pathonscript@gmail.com>
2024-06-18 21:05:06 +0700

    duolingo-inspired button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>SeatBox</h1>
-<p>จองโต๊ะเรียนออนไลน์</p>
+<main class="landing-container">
+	<h1>SeatBox</h1>
+	<p>จองโต๊ะเรียนออนไลน์</p>
+
+	<button>ล็อกอิน</button>
+</main>
+
+<style lang="scss">
+	.landing-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+</style>
